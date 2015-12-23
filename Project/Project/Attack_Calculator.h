45bab9faf7fa1a823ec222a6dcc5d0a6410b8213
TsungYing <alen6248@gmail.com>
2015-12-23 13:01:59 +0800

    2015/12/23 13:02 主要修改att_calculator, 將2D轉成1D問題

--- Project/Project/Attack_Calculator.h
+++ Project/Project/Attack_Calculator.h
@@ -1,16 +1,19 @@
 #pragma once
 //==========================設計===================================
-
+//得出給個塔的攻擊清單
+//將路徑拉成直線，利於計算 (2維轉成1維)
+//使用vector紀錄攻擊順序
+//
 //=================================================================
 
 
 
 
 /**************************問題***********************************/
-//得出給個塔的攻擊清單
-//Attack_Manager 功能是否設計在此class之內 ?
-//使用vector紀錄攻擊順序
 
+//Attack_Manager 功能是否設計在此class之內 ?
+//strong 定義為血量最多，加上weak定義為血量最少,加上攻擊模式:最慢?
+//將speed與hp融合成另一個變數"不好應付的程度" ?
 /*****************************************************************/
 
 
@@ -22,13 +25,16 @@ using namespace std;
 
 class Attack_Calculator {
 private:
-	enum Attack_Type{Strong,Fast,First,Last,Auto} att_type;
+	enum Attack_Type{Strong,Weak,Fast,First,Last,Auto} att_type;
 public:
 	Attack_Calculator();
 	~Attack_Calculator();
 	float tow_em_distance(Tower* tow,Enemy* em);
 	void enrol_att_list(Tower* tow, Enemy* em);
-
+	//將地圖與塔轉換成直線的函數  //須創造適當的container儲存
+	//得出塔攻擊涵蓋範圍資料
+	//得出塔的攻擊時刻與目標(1D)
+	//將塔的攻擊時刻與目標轉換成2D
 
 protected:
 };
