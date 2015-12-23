5fde814bc18408b85c35e86cce93e09465fa97de
TsungYing <alen6248@gmail.com>
2015-12-24 00:57:36 +0800

    2015/12/24 12:54 李宗穎

--- Project/Project/Result_Loader.h
+++ Project/Project/Result_Loader.h
@@ -2,7 +2,9 @@
 //==========================設計===================================
 ////Container
 //製作一種型態，儲存Progress所需資料
-
+//型態，儲存2D資料(放置塔的位置與敵人走的位置
+//型態，儲存地圖與塔2D轉成1D的資料(在直線的哪個地方有放塔)
+//型態，儲存直線的哪個區域是哪個塔的攻擊範圍
 
 //=================================================================
 
@@ -10,12 +12,71 @@
 
 
 /**************************問題***********************************/
-
+//型態，儲存1D轉回2D的資料(哪個位置需要轉彎)-->直接用2D資料??
 
 /*****************************************************************/
+
+#include "Tower.h"
+
+
+
+
+//型態，儲存2D資料(放置塔的位置與敵人走的位置
+//使用方法: vector<Tower_Location>
+enum Type_Exist { NonExist,Tower_Exist, Enemy_Exist }; //該型態是否存在物件
+enum Location_type{Tower_type,Enemy_type,None};  //該位置放置的型態
+//Location_type -> Type_Exist  
+//None 必須對應到 NonExist
+//Tower_type必須對應到 NonExist 或是 Tower_Exist
+//Enemy_type必須對應到 NonExist 或是 Enemy_Exist
+
+enum Turn {straigh, up, down,left,right };  //straight:直線無轉折
+
+struct Location{
+	int x;
+	int y;
+	Turn trun;
+
+	Location_type loctype;  
+	Type_Exist tpexist;
+	//Location_type -> Type_Exist  
+	//None 必須對應到 NonExist
+	//Tower_type必須對應到 NonExist 或是 Tower_Exist
+	//Enemy_type必須對應到 NonExist 或是 Enemy_Exist
+
+	Tower* tow;		//None  或是 NonExist 需對應到NULL
+	Enemy* em;		//None  或是 NonExist 需對應到NULL
+
+
+};
+
+
+
+
 
 
 
 //型態，儲存地圖與塔2D轉成1D的資料(在直線的哪個地方有放塔)
+struct Path_1D {
+	int d;
+	Turn turn;
+	Location_type loctype;
+	Type_Exist tpexist;
+	//Location_type -> Type_Exist  
+	//None 必須對應到 NonExist
+	//Tower_type必須對應到 NonExist 或是 Tower_Exist
+	//Enemy_type必須對應到 NonExist 或是 Enemy_Exist
+
+	Tower* tow;		//None  或是 NonExist 需對應到NULL
+	Enemy* em;		//None  或是 NonExist 需對應到NULL
+
+	struct Cover_by_Tower_Range {
+		vector<Tower*> cover_tow_set;
+
+	};
+};
 //型態，儲存直線的哪個區域是哪個塔的攻擊範圍
-//型態，儲存1D轉回2D的資料(哪個位置需要轉彎)
+struct Range_Tower_1D {
+
+};
+
--- Project/Project/Screen.h
+++ Project/Project/Screen.h
@@ -1,20 +1,13 @@
 #pragma once
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_mixer.h"  //還沒裝
+#include "stdio.h"
+#include "string"
+#include <vector>
 //==========================設計===================================
 ////引擎:SDL2
 ////遊戲介面、動畫效果
-
-
-
-//=================================================================
-
-
-
-
-/**************************問題***********************************/
-
-
-/*****************************************************************/
-
 //主畫面
 
 //地圖
@@ -28,17 +21,107 @@
 //Bullet攻擊動畫
 
 //點選塔出現選單與說明 
-	//塔的位置:升級資訊
-	//下方說明欄:目前塔的屬性與下一等級的屬性
+//塔的位置:升級資訊
+//下方說明欄:目前塔的屬性與下一等級的屬性
 
 //游標摸到塔出現浮動的簡略屬性資訊
 
 //游標移到敵人時顯示血條
 
 //點選敵人時
-	//敵人位置:出現血條
-	//下方說明欄:出現詳細敵人屬性資料
+//敵人位置:出現血條
+//下方說明欄:出現詳細敵人屬性資料
 
 //敵人進入城堡的動畫
 
+
+
+//=================================================================
+
+
+
+
+/**************************問題***********************************/
+
+
+/*****************************************************************/
+
+class Screen_Manager {
+
+public:
+	class LTexture {
+		//Initializes variables
+		LTexture();
+
+		//Deallocates memory
+		~LTexture();
+
+		//Loads image at specified path
+		bool loadFromFile(std::string path);
+
+		#ifdef _SDL_TTF_H			//這幹嘛用的??
+		//Creates image from font string
+		bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
+		#endif 
+
+		//Deallocates texture
+		void free_texture();
+
+		//Set color modulation
+		void setColor(Uint8 red, Uint8 green, Uint8 blue);
+
+		//Set blending
+		void setBlendMode(SDL_BlendMode blending);
+
+		//Set alpha modulation
+		void setAlpha(Uint8 alpha);
+
+		//Renders texture at given point
+		void render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+		//Gets image dimensions
+		int getWidth();
+		int getHeight();
+
+	private:
+		//The actual hardware texture
+		SDL_Texture* mTexture;
+
+		//Image dimensions
+		int mWidth;
+		int mHeight;
+	};
+	//需再宣告LTexture物件
+	LTexture Map;
+	LTexture Main_Screen;
+	vector<LTexture> Texture_Set; 
+	vector<LTexture> Enemy_Set;
+	LTexture castle;
+	LTexture Bullet;
+
+
+
+
+	//Starts up SDL and creates window
+	bool init();
+
+	//Loads media
+	bool loadMedia();
+
+	//Frees media and shuts down SDL
+	void close();
+
+	//The window we'll be rendering to
+	SDL_Window* gWindow = NULL;
+
+	//The window renderer
+	SDL_Renderer* gRenderer = NULL;
+
+	//Scene texture
+	LTexture gPromptTexture;
+
+
+
+
+};
 
